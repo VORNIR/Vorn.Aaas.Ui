53005b8c3da4e4b0d06a61d5b58eafd8e1116a31
Mobin <mobin.seven@live.com>
2022-10-11 18:49:08 +0330

    v0.2.2

--- Vorn.Aaas.Ui/Components/PageDetails.cs
+++ Vorn.Aaas.Ui/Components/PageDetails.cs
@@ -4,10 +4,12 @@ public class PageDetails : Attribute
     public string Route { get; init; }
     public string Title { get; init; }
     public string? Icon { get; init; }
-    public PageDetails(string route, string title, string? icon = null)
+    public int? Order { get; init; }
+    public PageDetails(string route, string title, string? icon = null, int? order = null)
     {
         Route = route;
         Title = title;
         Icon = icon;
+        Order = order;
     }
 }
